a5fe4a873a7db82d5bd1df978a89fd8b8e6fc8bf
breakItNow <49260173+breakItNow@users.noreply.github.com>
2019-07-01 17:24:41 +0800

    Update Hello.java

--- GitCon/src/com/test/Hello.java
+++ GitCon/src/com/test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Git loading");
 		System.out.println("Test again");
+		System.out.println("Test twice again");
 	}
 }
